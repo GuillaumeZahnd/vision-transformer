0bc6514d0191854fbc00142656c8a23934cc5e05
gzahnd <>
2025-11-24 14:14:19 +0100

    Add ALiBi multi head self attention

---
+++ alibi_multi_head_self_attention.py
@@ -0,0 +1,55 @@
+from get_alibi import get_alibi
+
+import torch
+import torch.nn as nn
+import math
+
+
+class AlibiMultiHeadSelfAttention(nn.Module):
+    def __init__(self, embedding_dim:int, nb_heads:int, nb_patches_per_side: int) -> None:
+        super().__init__()
+
+        assert embedding_dim % nb_heads == 0, "The embedding dimension must be a multiple of the number of heads."
+
+        self.nb_heads = nb_heads
+        self.head_dim = embedding_dim // nb_heads
+
+        self.q_weights = nn.Linear(embedding_dim, embedding_dim, bias=False)
+        self.k_weights = nn.Linear(embedding_dim, embedding_dim, bias=False)
+        self.v_weights = nn.Linear(embedding_dim, embedding_dim, bias=False)
+        self.linear = nn.Linear(embedding_dim, embedding_dim)
+
+        # ALiBi is used for positional encoding
+        alibi = get_alibi(nb_heads=self.nb_heads, nb_patches_per_side=nb_patches_per_side)
+        self.register_buffer("alibi", alibi, persistent=False)
+
+
+    def forward(self, sequence_embedding: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the multi-head self attention, with the ALiBi positional encoding.
+        See: Press et al., Train Short, Test Long, ICLR 2022 (https://arxiv.org/pdf/2108.12409).
+
+        Args:
+            sequence_embedding: Input sequence embedding, in the shape (batch_size, nb_patches_per_side^2 +1, embedding_dim)
+
+        Returns:
+            Weighted sum of values, in the shape (batch_size, nb_patches_per_side^2 +1, embedding_dim)
+            Attention weights, in the shape (batch_size, nb_heads, nb_patches_per_side^2 +1, nb_patches_per_side^2 +1)
+        """
+
+        batch_size, sequence_length, embedding_dim = sequence_embedding.shape
+
+        q = self.q_weights(sequence_embedding).view(batch_size, sequence_length, self.nb_heads, self.head_dim).transpose(1, 2)
+        k = self.k_weights(sequence_embedding).view(batch_size, sequence_length, self.nb_heads, self.head_dim).transpose(1, 2)
+        v = self.v_weights(sequence_embedding).view(batch_size, sequence_length, self.nb_heads, self.head_dim).transpose(1, 2)
+
+        scaled_attention_logits = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(q.size(-1))
+
+        # Crucially, the positional encoding is applied here
+        attention = torch.softmax(scaled_attention_logits + self.alibi, dim=-1)
+
+        values = torch.matmul(attention, v)
+        values = values.transpose(1, 2).contiguous().view((batch_size, sequence_length, embedding_dim))
+        values = self.linear(values)
+
+        return values, attention
--- get_alibi.py
+++ get_alibi.py
@@ -4,63 +4,63 @@ import itertools
 from typing import List
 
 
-def get_alibi(number_of_attention_heads: int, number_of_patches_per_side: int) -> torch.Tensor:
+def get_alibi(number_of_heads: int, number_of_patches_per_side: int) -> torch.Tensor:
     """
     Compute the linear biases required for ALiBi.
     See: Press et al., Train Short, Test Long, ICLR 2022 (https://arxiv.org/pdf/2108.12409).
 
     Args:
-        number_of_attention_heads: Number of attention heads.
+        number_of_heads: Number of attention heads.
         number_of_patches_per_side: Number of patches along each side of the image.
 
     Returns:
-        Linear biases, in the shape (1, number_of_attention_heads, number_of_patches_per_side +1, number_of_patches_per_side +1).
+        Linear biases, in the shape (1, number_of_heads, number_of_patches_per_side +1, number_of_patches_per_side +1).
     """
 
-    def get_slope(number_of_attention_heads: int) -> torch.Tensor:
+    def get_slope(number_of_heads: int) -> torch.Tensor:
         """
             Compute a slope, which represents decreasing head-specific coefficients.
             Inspired from https://github.com/ofirpress/attention_with_linear_biases/blob/4b92f28a005ead2567abe2359f633e73e08f3833/fairseq/models/transformer.py#L742
 
             Args:
-                number_of_attention_heads: Number of attention heads.
+                number_of_heads: Number of attention heads.
 
             Returns:
-                Slope, in the shape (number_of_attention_heads).
+                Slope, in the shape (number_of_heads).
         """
 
-        def get_slope_power_of_2(number_of_attention_heads: int) -> List[int]:
+        def get_slope_power_of_2(number_of_heads: int) -> List[int]:
             """
             Compute the slope coefficients, as a geometric series starting from O.5^8.
 
             Args:
-                number_of_attention_heads: Number of attention heads.
+                number_of_heads: Number of attention heads.
 
             Returns:
-                List of slope coefficients, of length (number_of_attention_heads).
+                List of slope coefficients, of length (number_of_heads).
 
             """
 
-            # The starting value is effectively "2^-(8/number_of_attention_heads)", up to numerical precision.
-            start = (2 ** (-2 ** -(math.log2(number_of_attention_heads) - 3)))
+            # The starting value is effectively "2^-(8/number_of_heads)", up to numerical precision.
+            start = (2 ** (-2 ** -(math.log2(number_of_heads) - 3)))
 
             ratio = start
-            return [start * ratio ** i for i in range(number_of_attention_heads)]
+            return [start * ratio ** i for i in range(number_of_heads)]
 
-        if math.log2(number_of_attention_heads).is_integer():
-            slope = get_slope_power_of_2(number_of_attention_heads)
+        if math.log2(number_of_heads).is_integer():
+            slope = get_slope_power_of_2(number_of_heads)
 
         else:
-            closest_smaller_power_of_2 = 2 ** math.floor(math.log2(number_of_attention_heads))
+            closest_smaller_power_of_2 = 2 ** math.floor(math.log2(number_of_heads))
             slope = \
                 get_slope_power_of_2(closest_smaller_power_of_2) + \
-                get_slope(2 * closest_smaller_power_of_2)[0::2][:number_of_attention_heads - closest_smaller_power_of_2]
+                get_slope(2 * closest_smaller_power_of_2)[0::2][:number_of_heads - closest_smaller_power_of_2]
 
         slope = torch.tensor(slope, dtype=torch.float32)
         slope, _ = torch.sort(slope, dim=0, descending=True)
         return slope
 
-    slope = get_slope(number_of_attention_heads=number_of_attention_heads)
+    slope = get_slope(number_of_heads=number_of_heads)
 
 
     def get_distances(number_of_patches_per_side: int) -> torch.Tensor:
