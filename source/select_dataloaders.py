3502f8a26d918dd6e4928ac8d7ce78fe5bcff8f0
gzahnd <>
2025-11-28 14:47:06 +0100

    Update

--- source/select_dataloaders.py
+++ source/select_dataloaders.py
@@ -4,10 +4,12 @@ from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor
 from torch.utils.data import random_split
+from torchvision.datasets import CIFAR10
 from torchvision.datasets import CIFAR100
 
 
 class Datasets(Enum):
+    CIFAR10 = "CIFAR10"
     CIFAR100 = "CIFAR100"
 
 
@@ -28,7 +30,27 @@ def select_dataloaders(cfg: DictConfig) -> tuple[DataLoader, DataLoader, DataLoa
         return dataset_training, dataset_validation
 
 
-    if dataset_nickname == Datasets.CIFAR100.value:
+    if dataset_nickname == Datasets.CIFAR10.value:
+        dataset_training = CIFAR10(root="datasets", download=True, train=True, transform=ToTensor())
+        dataset_test = CIFAR10(root="datasets", download=True, train=False, transform=ToTensor())
+
+        dataset_training, dataset_validation = _split_validation_from_training(
+            dataset_training=dataset_training,
+            training_validation_split=cfg.dataset.training_validation_split)
+
+        dataloader_training = DataLoader(
+            dataset_training, batch_size=cfg.training.batch_size, shuffle=True,
+            num_workers=cfg.environment.num_workers, persistent_workers=True, prefetch_factor=16)
+        dataloader_validation = DataLoader(
+            dataset_validation, batch_size=cfg.training.batch_size, shuffle=False,
+            num_workers=cfg.environment.num_workers, persistent_workers=True, prefetch_factor=16)
+        dataloader_test = DataLoader(
+            dataset_test, batch_size=cfg.training.batch_size, shuffle=False,
+            num_workers=cfg.environment.num_workers, persistent_workers=True, prefetch_factor=16)
+
+        return dataloader_training, dataloader_validation, dataloader_test
+
+    elif dataset_nickname == Datasets.CIFAR100.value:
         dataset_training = CIFAR100(root="datasets", download=True, train=True, transform=ToTensor())
         dataset_test = CIFAR100(root="datasets", download=True, train=False, transform=ToTensor())
 
